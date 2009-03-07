54a6e83ae17602dc0ab39f13fb6c72833010f923
sdorra <sdorra@localhost>
2009-03-07 16:55:48 +0100

    [svn r113] improve CodeMacro

--- plugins/OfficePlugin/src/sonia/blog/office/CodeMacro.java
+++ plugins/OfficePlugin/src/sonia/blog/office/CodeMacro.java
@@ -26,17 +26,28 @@ public class CodeMacro extends AbstractBlogMacro
   /** Field description */
   public static final String NAME = "code";
 
+  /** Field description */
+  private static final String CSS_PREFIX =
+    "<link type=\"text/css\" rel=\"stylesheet\" href=\"";
+
+  /** Field description */
+  private static final String JS_PREFIX =
+    "<script language=\"javascript\" src=\"";
+
+  /** Field description */
+  private static final String SUFFIX = "\"></script>\n";
+
   //~--- set methods ----------------------------------------------------------
 
   /**
    * Method description
    *
    *
-   * @param controls
+   * @param collapse
    */
-  public void setControls(String controls)
+  public void setCollapse(String collapse)
   {
-    this.controls = controls;
+    this.collapse = collapse;
   }
 
   /**
@@ -61,6 +72,28 @@ public class CodeMacro extends AbstractBlogMacro
     this.lang = lang;
   }
 
+  /**
+   * Method description
+   *
+   *
+   * @param tabSize
+   */
+  public void setTabSize(String tabSize)
+  {
+    this.tabSize = tabSize;
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @param toolbar
+   */
+  public void setToolbar(String toolbar)
+  {
+    this.toolbar = toolbar;
+  }
+
   //~--- methods --------------------------------------------------------------
 
   /**
@@ -78,100 +111,133 @@ public class CodeMacro extends AbstractBlogMacro
   protected String doBody(FacesContext facesContext, String linkBase,
                           ContentObject object, String body)
   {
-    linkBase += "resource/dp/syntax/";
+    StringBuffer jsPrefix = new StringBuffer();
 
-    String result = "";
+    jsPrefix.append(JS_PREFIX).append(linkBase);
+    jsPrefix.append("resource/syntax/scripts/");
 
-    result += "<link type=\"text/css\" rel=\"stylesheet\" href=\"" + linkBase
-              + "css/SyntaxHighlighter.css\"></link>\n";
-    result += "<script language=\"javascript\" src=\"" + linkBase
-              + "js/shCore.js\"></script>\n";
+    StringBuffer cssPrefix = new StringBuffer();
+
+    cssPrefix.append(CSS_PREFIX).append(linkBase);
+    cssPrefix.append("resource/syntax/styles/");
+
+    StringBuffer result = new StringBuffer();
+
+    // Core CSS
+    result.append(cssPrefix).append("shCore.css").append(SUFFIX);
+    result.append(cssPrefix).append("shThemeDefault.css").append(SUFFIX);
+
+    // Core JS
+    result.append(jsPrefix).append("shCore.js").append(SUFFIX);
+    result.append(jsPrefix).append("shBrushXml.js").append(SUFFIX);
     lang = lang.toLowerCase();
 
-    if (lang.equals("csharp") || lang.equals("c#") || lang.equals("c-sharp"))
+    if (lang.equals("bash"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushCsharp.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushBash.js").append(SUFFIX);
+    }
+    else if (lang.equals("csharp") || lang.equals("c#")
+             || lang.equals("c-sharp"))
+    {
+      result.append(jsPrefix).append("shBrushCSharp.js").append(SUFFIX);
     }
     else if (lang.equals("cpp") || lang.equals("c++") || lang.equals("c"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushCpp.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushCpp.js").append(SUFFIX);
     }
     else if (lang.equals("css"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushCss.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushCss.js").append(SUFFIX);
     }
     else if (lang.equals("delphi") || lang.equals("pascal"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushDelphi.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushDelphi.js").append(SUFFIX);
+    }
+    else if (lang.equals("diff"))
+    {
+      result.append(jsPrefix).append("shBrushDiff.js").append(SUFFIX);
+    }
+    else if (lang.equals("groovy"))
+    {
+      result.append(jsPrefix).append("shBrushGroovy.js").append(SUFFIX);
     }
     else if (lang.equals("js") || lang.equals("javascript")
              || lang.equals("jscript"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushJScript.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushJScript.js").append(SUFFIX);
     }
     else if (lang.equals("java"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushJava.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushJava.js").append(SUFFIX);
+    }
+    else if (lang.equals("perl"))
+    {
+      result.append(jsPrefix).append("shBrushPerl.js").append(SUFFIX);
     }
     else if (lang.equals("php"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushPhp.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushPhp.js").append(SUFFIX);
+    }
+    else if (lang.equals("plain"))
+    {
+      result.append(jsPrefix).append("shBrushPlain.js").append(SUFFIX);
     }
     else if (lang.equals("python") || lang.equals("py"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushPython.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushPython.js").append(SUFFIX);
     }
     else if (lang.equals("ruby") || lang.equals("ror") || lang.equals("rb")
              || lang.equals("rails"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushRuby.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushRuby.js").append(SUFFIX);
+    }
+    else if (lang.equals("scala"))
+    {
+      result.append(jsPrefix).append("shBrushScala.js").append(SUFFIX);
     }
     else if (lang.equals("sql"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushSql.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushSql.js").append(SUFFIX);
     }
     else if (lang.equals("vb") || lang.equals("vb.net"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushVb.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushVb.js").append(SUFFIX);
     }
     else if (lang.equals("xml") || lang.equals("html") || lang.equals("xhtml")
              || lang.equals("xslt"))
     {
-      result += "<script language=\"javascript\" src=\"" + linkBase
-                + "js/shBrushXml.js\"></script>\n";
+      result.append(jsPrefix).append("shBrushXml.js").append(SUFFIX);
     }
 
-    result += "<pre name=\"code\" class=\"" + lang + "\">\n";
-    result += body;
-    result += "</pre>\n";
-    result += "<script language=\"javascript\">\n";
-    result += "dp.SyntaxHighlighter.BloggerMode();\n";
-    result += "dp.SyntaxHighlighter.HighlightAll('code', " + gutter + ", "
-              + controls + ", false, 1, false);\n";
-    result += "</script>\n";
+    result.append("<script language=\"javascript\">\n");
+    result.append("SyntaxHighlighter.config.clipboardSwf = '");
+    result.append(linkBase).append("resource/syntax/scripts/clipboard.swf';\n");
+    result.append("SyntaxHighlighter.config.bloggerMode = true;\n");
+    result.append("SyntaxHighlighter.all();\n");
+    result.append("</script>\n");
+    result.append("<pre name=\"code\" class=\"brush: ").append(lang);
+    result.append("; toolbar: ").append(toolbar).append("; gutter: ");
+    result.append(gutter).append("; collapse: ").append(collapse);
+    result.append("; tab-size: ").append(tabSize);
+    result.append(";\">\n").append(body).append("\n</pre>\n");
 
-    return result;
+    return result.toString();
   }
 
   //~--- fields ---------------------------------------------------------------
 
   /** Field description */
-  private String controls = "false";
+  private String collapse = "false";
 
   /** Field description */
-  private String gutter = "false";
+  private String gutter = "true";
 
   /** Field description */
   private String lang = "java";
+
+  /** Field description */
+  private String tabSize = "2";
+
+  /** Field description */
+  private String toolbar = "true";
 }
