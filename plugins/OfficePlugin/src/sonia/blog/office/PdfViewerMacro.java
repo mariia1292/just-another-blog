8bb819f1baae3a68ba05465e5d29b696e4a39d5f
sdorra <sdorra@localhost>
2009-04-11 12:03:09 +0200

    improve PdfViewerMacro

--- plugins/OfficePlugin/src/sonia/blog/office/PdfViewerMacro.java
+++ plugins/OfficePlugin/src/sonia/blog/office/PdfViewerMacro.java
@@ -13,6 +13,7 @@ import sonia.blog.api.app.BlogContext;
 import sonia.blog.api.app.BlogRequest;
 import sonia.blog.api.app.Constants;
 import sonia.blog.api.dao.AttachmentDAO;
+import sonia.blog.api.dao.Dao;
 import sonia.blog.api.util.AbstractBlogMacro;
 import sonia.blog.entity.Attachment;
 import sonia.blog.entity.Blog;
@@ -66,7 +67,7 @@ public class PdfViewerMacro extends AbstractBlogMacro
    *
    * @param id
    */
-  public void setId(String id)
+  public void setId(Long id)
   {
     this.id = id;
   }
@@ -89,17 +90,6 @@ public class PdfViewerMacro extends AbstractBlogMacro
                           ContentObject object, String body)
   {
     String result = null;
-
-    if (extension == null)
-    {
-      extension = Constants.DEFAULT_IMAGE_EXTENSION;
-    }
-
-    if (format == null)
-    {
-      format = Constants.DEFAULT_IMAGE_FORMAT;
-    }
-
     Blog blog = getCurrentBlog(facesContext);
     BlogRequest request = getRequest(facesContext);
     BlogContext context = BlogContext.getInstance();
@@ -108,11 +98,7 @@ public class PdfViewerMacro extends AbstractBlogMacro
     {
       if (id != null)
       {
-        Long attachmentId = Long.parseLong(id);
-        AttachmentDAO attachmentDAO =
-          BlogContext.getDAOFactory().getAttachmentDAO();
-        Attachment attachment = attachmentDAO.findByBlogAndId(blog,
-                                  attachmentId);
+        Attachment attachment = attachmentDAO.findByBlogAndId(blog, id);
 
         if (attachment.getMimeType().equalsIgnoreCase(PDFMIMETYPE))
         {
@@ -121,7 +107,7 @@ public class PdfViewerMacro extends AbstractBlogMacro
 
           if (attachmentFile.exists())
           {
-            result = createPdfImageGallery(request, linkBase, attachmentId,
+            result = createPdfImageGallery(request, linkBase, id,
                                            attachmentFile, body);
           }
           else
@@ -280,18 +266,18 @@ public class PdfViewerMacro extends AbstractBlogMacro
   private String printPdfImageGallery(BlogRequest request, String linkBase,
           File pdfDir, String body)
   {
-    String result = "";
+    StringBuffer result = new StringBuffer();
     String res = linkBase + "resources/lightbox/";
 
     if (request.getAttribute("sonia.blog.macro.gallery") == null)
     {
-      result += "<script type=\"text/javascript\" src=\"" + res
-                + "js/jquery.js\"></script>\n";
-      result += "<script type=\"text/javascript\" src=\"" + res
-                + "js/jquery.lightbox-0.5.js\"></script>\n";
-      result += "<link rel=\"stylesheet\" href=\"" + res
-                + "css/jquery.lightbox-0.5.css\" "
-                + "type=\"text/css\" media=\"screen\"></link>\n";
+      result.append("<script type=\"text/javascript\" src=\"").append(res);
+      result.append("js/jquery.js\"></script>\n");
+      result.append("<script type=\"text/javascript\" src=\"").append(res);
+      result.append("js/jquery.lightbox-0.5.js\"></script>\n");
+      result.append("<link rel=\"stylesheet\" href=\"").append(res);
+      result.append("css/jquery.lightbox-0.5.css\" ");
+      result.append("type=\"text/css\" media=\"screen\"></link>\n");
       request.setAttribute("sonia.blog.macro.gallery", Boolean.TRUE);
     }
 
@@ -304,7 +290,7 @@ public class PdfViewerMacro extends AbstractBlogMacro
     });
     String name = "pdfgallery_" + pdfDir.getName();
 
-    result += "<span id=\"" + name + "\">\n";
+    result.append("<span id=\"").append(name).append("\">\n");
 
     String path = pdfDir.getName() + "/";
     String baseLink = linkBase + "macros/pdfviewer/" + path;
@@ -315,54 +301,68 @@ public class PdfViewerMacro extends AbstractBlogMacro
       {
         String link = baseLink + i + "." + extension;
 
-        result += "<a id=\"" + name + "_" + i + "\" title=\"Page " + i + "\" ";
-        result += "rel=\"[ligthbox[group_a" + i + "]\" href=\"" + link + "\"";
+        result.append("<a id=\"").append(name).append("_").append(i);
+        result.append("\" title=\"Page ").append(i).append("\" ");
+        result.append("rel=\"[ligthbox[group_a").append(i);
+        result.append("]\" href=\"").append(link).append("\"");
 
         if (i > 1)
         {
-          result += " style=\"display: none;\">";
+          result.append(" style=\"display: none;\">");
         }
         else
         {
           if ((body == null) || (body.trim().length() == 0))
           {
-            body = "<img src=\"" + linkBase
-                   + "resource/icon/document.gif\" alt=\"PDF\" />";
+            StringBuffer bodyBuffer = new StringBuffer();
+
+            bodyBuffer.append("<img src=\"").append(linkBase);
+            bodyBuffer.append("resource/icon/document.gif\" alt=\"PDF\" />");
+            body = bodyBuffer.toString();
           }
 
-          result += ">" + body;
+          result.append(">").append(body);
         }
 
-        result += "</a>\n";
+        result.append("</a>\n");
       }
     }
 
-    result += "</span>\n";
-    result += "<script type=\"text/javascript\">\n";
-    result += "$(document).ready(function() {\n";
-    result += "$(\"span#" + name + " a\").lightBox({\n";
-    result += "imageLoading: '" + res + "images/lightbox-ico-loading.gif',\n";
-    result += "imageBtnPrev: '" + res + "images/lightbox-btn-prev.gif',\n";
-    result += "imageBtnNext: '" + res + "images/lightbox-btn-next.gif',\n";
-    result += "imageBtnClose: '" + res + "images/lightbox-btn-close.gif',\n";
-    result += "imageBlank: '" + res + "images/lightbox-blank.gif'\n";
-    result += "});\n";
-    result += "});\n";
-    result += "</script>\n";
+    result.append("</span>\n");
+    result.append("<script type=\"text/javascript\">\n");
+    result.append("$(document).ready(function() {\n");
+    result.append("$(\"span#").append(name).append(" a\").lightBox({\n");
+    result.append("imageLoading: '").append(res);
+    result.append("images/lightbox-ico-loading.gif',\n");
+    result.append("imageBtnPrev: '").append(res);
+    result.append("images/lightbox-btn-prev.gif',\n");
+    result.append("imageBtnNext: '").append(res);
+    result.append("images/lightbox-btn-next.gif',\n");
+    result.append("imageBtnClose: '").append(res);
+    result.append("images/lightbox-btn-close.gif',\n");
+    result.append("imageBlank: '").append(res);
+    result.append("images/lightbox-blank.gif'\n");
+    result.append("});\n");
+    result.append("});\n");
+    result.append("</script>\n");
 
-    return result;
+    return result.toString();
   }
 
   //~--- fields ---------------------------------------------------------------
 
   /** Field description */
+  @Dao
+  private AttachmentDAO attachmentDAO;
+
+  /** Field description */
   @Config(Constants.CONFIG_IMAGEEXTENSION)
-  private String extension;
+  private String extension = Constants.DEFAULT_IMAGE_EXTENSION;
 
   /** Field description */
   @Config(Constants.CONFIG_IMAGEFORMAT)
-  private String format;
+  private String format = Constants.DEFAULT_IMAGE_FORMAT;
 
   /** Field description */
-  private String id;
+  private Long id;
 }
