8acdc1e31b48942982e6f235b04235761827a96c
sdorra <sdorra@localhost>
2009-04-17 15:54:36 +0200

    added navigation for pages

--- SoniaBlogAPI/src/sonia/blog/entity/Page.java
+++ SoniaBlogAPI/src/sonia/blog/entity/Page.java
@@ -43,6 +43,56 @@ public class Page implements Serializable, PermaObject, ContentObject
    * Method description
    *
    *
+   * @param obj
+   *
+   * @return
+   */
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (obj == null)
+    {
+      return false;
+    }
+
+    if (getClass() != obj.getClass())
+    {
+      return false;
+    }
+
+    final Page other = (Page) obj;
+
+    if ((this.id != other.id)
+        && ((this.id == null) ||!this.id.equals(other.id)))
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Method description
+   *
+   *
+   * @return
+   */
+  @Override
+  public int hashCode()
+  {
+    int hash = 7;
+
+    hash = 79 * hash + ((this.id != null)
+                        ? this.id.hashCode()
+                        : 0);
+
+    return hash;
+  }
+
+  /**
+   * Method description
+   *
+   *
    * @return
    */
   public boolean renderMacros()
