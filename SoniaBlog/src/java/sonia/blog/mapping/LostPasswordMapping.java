7e3cfb68e65053416a47b88ccb0e6a92b105ee20
sdorra <sdorra@localhost>
2009-05-06 18:13:10 +0200

    added navigation for lost password page

--- SoniaBlog/src/java/sonia/blog/mapping/LostPasswordMapping.java
+++ SoniaBlog/src/java/sonia/blog/mapping/LostPasswordMapping.java
@@ -102,6 +102,8 @@ public class LostPasswordMapping extends FinalMapping
             if (userDAO.edit(user))
             {
               sendMail(blog, locale, user, password);
+
+              // TODO: add Message
             }
           }
           catch (Exception ex)
