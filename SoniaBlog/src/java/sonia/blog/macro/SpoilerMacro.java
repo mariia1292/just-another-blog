857d45fe58c222be7bd1a1b82f3f5da902269bf6
sdorra <sdorra@localhost>
2009-05-06 18:24:11 +0200

    added support for submacros to the spoiler macro and added the pre macro

--- SoniaBlog/src/java/sonia/blog/macro/SpoilerMacro.java
+++ SoniaBlog/src/java/sonia/blog/macro/SpoilerMacro.java
@@ -9,6 +9,7 @@ package sonia.blog.macro;
 
 //~--- non-JDK imports --------------------------------------------------------
 
+import sonia.blog.api.app.BlogContext;
 import sonia.blog.entity.ContentObject;
 
 import sonia.macro.Macro;
@@ -41,40 +42,60 @@ public class SpoilerMacro implements Macro
     }
 
     ContentObject object = (ContentObject) environment.get("object");
-    String js = "var div = this.parentNode.getElementsByTagName('div')[0]";
+    StringBuffer js = new StringBuffer();
 
-    js += ".getElementsByTagName('div')[0];";
-    js += "if ( div.style.display != '' ){ div.style.display = '' } ";
-    js += "else { div.style.display = 'none' }";
+    js.append("var div = this.parentNode.getElementsByTagName('div')[0]");
+    js.append(".getElementsByTagName('div')[0];");
+    js.append("if ( div.style.display != '' ){ div.style.display = '' } ");
+    js.append("else { div.style.display = 'none' }");
 
-    String result = "<div>\n";
+    StringBuffer result = new StringBuffer();
 
-    result += "<input type=\"button\" value=\"" + title + "\" onclick=\"" + js
-              + "\" />\n";
-    result += "<div";
+    result.append("<div>\n");
+    result.append("<input type=\"button\" value=\"").append(title);
+    result.append("\" onclick=\"").append(js).append("\" />\n");
+    result.append("<div");
 
     if (style != null)
     {
-      result += " style=\"" + style + "\"";
+      result.append(" style=\"").append(style).append("\"");
     }
 
     if (styleClass != null)
     {
-      result += " class=\"" + styleClass + "\"";
+      result.append(" class=\"").append(styleClass).append("\"");
     }
 
-    result += ">";
-    result += "<div id=\"spoiler_" + object.getId()
-              + "\" style=\"display: none;\">\n";
-    result += body + "\n";
-    result += "</div>\n";
-    result += "</div>\n";
-    result += "</div>\n";
+    result.append(">");
+    result.append("<div id=\"spoiler_").append(object.getId());
+    result.append("\" style=\"display: none;\">\n");
 
-    return result;
+    if (macros)
+    {
+      body = BlogContext.getInstance().getMacroParser().parseText(environment,
+              body);
+    }
+
+    result.append(body).append("\n");
+    result.append("</div>\n");
+    result.append("</div>\n");
+    result.append("</div>\n");
+
+    return result.toString();
   }
 
   //~--- set methods ----------------------------------------------------------
+
+  /**
+   * Method description
+   *
+   *
+   * @param macros
+   */
+  public void setMacros(Boolean macros)
+  {
+    this.macros = macros;
+  }
 
   /**
    * Method description
@@ -110,6 +131,9 @@ public class SpoilerMacro implements Macro
   }
 
   //~--- fields ---------------------------------------------------------------
+
+  /** Field description */
+  private Boolean macros = Boolean.TRUE;
 
   /** Field description */
   private String style;
