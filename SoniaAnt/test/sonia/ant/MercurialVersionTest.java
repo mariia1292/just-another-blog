9f7704ccc96a14e9084a0c18be032ac274795736
sdorra <sdorra@localhost>
2009-05-07 15:57:57 +0200

    create SoniaAnt project for custom ant tasks and created MercurialVersion task

---
+++ SoniaAnt/test/sonia/ant/MercurialVersionTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sonia.ant;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sdorra
+ */
+public class MercurialVersionTest {
+
+    public MercurialVersionTest() {
+    }
+
+  /**
+   * Test of execute method, of class MercurialVersion.
+   */
+ 
+    @Test
+    public void test()
+    {
+
+      MercurialVersion hgVersion = new MercurialVersion();
+      hgVersion.execute();
+
+    }
+
+}
