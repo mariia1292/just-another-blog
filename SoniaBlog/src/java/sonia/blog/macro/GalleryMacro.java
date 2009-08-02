bbf0438b74a23d35e1435cdf38db219f99b470a2
sdorra <sdorra@localhost>
2009-08-02 13:09:40 +0200

    change to freemarker template

--- SoniaBlog/src/java/sonia/blog/macro/GalleryMacro.java
+++ SoniaBlog/src/java/sonia/blog/macro/GalleryMacro.java
@@ -59,9 +59,9 @@ import sonia.util.Util;
 //~--- JDK imports ------------------------------------------------------------
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Map;
 
 /**
  *
@@ -77,7 +77,8 @@ public class GalleryMacro extends AbstractBlogMacro implements WebMacro
 {
 
   /** Field description */
-  private static Logger logger = Logger.getLogger(GalleryMacro.class.getName());
+  private static final String TEMPLATE =
+    "sonia/blog/macro/template/gallery.html";
 
   //~--- get methods ----------------------------------------------------------
 
@@ -129,81 +130,45 @@ public class GalleryMacro extends AbstractBlogMacro implements WebMacro
   protected String doBody(BlogRequest request, String linkBase,
                           ContentObject object, String body)
   {
-    StringBuffer result = new StringBuffer();
+    String result = "";
     List<Attachment> images = getImages(object);
 
     if (Util.hasContent(images))
     {
-      try
-      {
-        if ((images != null) &&!images.isEmpty())
-        {
-          LinkBuilder linkBuilder = BlogContext.getInstance().getLinkBuilder();
-          String res = linkBuilder.buildLink(request, "/resources/");
-          String lRes = res + "prettyPhoto/";
+      LinkBuilder linkBuilder = BlogContext.getInstance().getLinkBuilder();
+      String res = linkBuilder.buildLink(request, "/resources/");
+      String lRes = res + "prettyPhoto/";
 
-          resources = new ArrayList<WebResource>();
+      resources = new ArrayList<WebResource>();
 
-          ScriptResource jquery = new ScriptResource(20,
-                                    res + "jquery/jquery.min.js");
+      ScriptResource jquery = new ScriptResource(20,
+                                res + "jquery/jquery.min.js");
 
-          resources.add(jquery);
+      resources.add(jquery);
 
-          ScriptResource jqueryPrettyPhoto = new ScriptResource(21,
-                                               lRes
-                                               + "js/jquery.prettyPhoto.js");
+      ScriptResource jqueryPrettyPhoto = new ScriptResource(21,
+                                           lRes + "js/jquery.prettyPhoto.js");
 
-          resources.add(jqueryPrettyPhoto);
+      resources.add(jqueryPrettyPhoto);
 
-          LinkResource prettyPhotoCSS = new LinkResource(22);
+      LinkResource prettyPhotoCSS = new LinkResource(22);
 
-          prettyPhotoCSS.setRel(LinkResource.REL_STYLESHEET);
-          prettyPhotoCSS.setType(LinkResource.TYPE_STYLESHEET);
-          prettyPhotoCSS.setHref(lRes + "css/prettyPhoto.css");
-          resources.add(prettyPhotoCSS);
+      prettyPhotoCSS.setRel(LinkResource.REL_STYLESHEET);
+      prettyPhotoCSS.setType(LinkResource.TYPE_STYLESHEET);
+      prettyPhotoCSS.setHref(lRes + "css/prettyPhoto.css");
+      resources.add(prettyPhotoCSS);
 
-          long time = System.nanoTime();
+      long time = System.nanoTime();
+      Map<String, Object> parameters = new HashMap<String, Object>();
 
-          result.append("<div id=\"gallery_").append(time);
-          result.append("\">\n");
-
-          for (int i = 0; i < images.size(); i++)
-          {
-            Attachment image = images.get(i);
-
-            result.append("<a title=\"");
-            result.append(image.getName()).append(
-                "\" rel=\"prettyPhoto[group_");
-            result.append(time).append("]\" href=\"");
-            result.append(linkBase).append("image/").append(image.getId());
-            result.append("\">\n").append("<img border=\"0\" alt=\"\" src=\"");
-            result.append(linkBase).append("image/").append(image.getId());
-            result.append("?type=thumb\" />\n").append("</a>");
-          }
-
-          result.append("</div>\n");
-          result.append(
-              "<script type=\"text/javascript\" charset=\"utf-8\">\n");
-
-          // on document ready
-          result.append("$(document).ready(function() {\n");
-
-          // configure gallery
-          result.append("$(\"div#gallery_").append(time);
-          result.append(" a\").prettyPhoto({\n");
-          result.append("theme: '").append(theme).append("'\n");
-          result.append("});\n");
-          result.append("});\n");
-          result.append("</script>\n");
-        }
-      }
-      catch (Exception ex)
-      {
-        logger.log(Level.SEVERE, null, ex);
-      }
+      parameters.put("linkBase", linkBase);
+      parameters.put("id", String.valueOf(time));
+      parameters.put("theme", theme);
+      parameters.put("images", images);
+      result = parseTemplate(parameters, TEMPLATE);
     }
 
-    return result.toString();
+    return result;
   }
 
   //~--- get methods ----------------------------------------------------------
