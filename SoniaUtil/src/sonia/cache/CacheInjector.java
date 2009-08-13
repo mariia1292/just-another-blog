25a731b77dd024eb2d0e568da497798751415513
sdorra <sdorra@localhost>
2009-08-13 08:21:05 +0200

    added Cache and CacheInjector

---
+++ SoniaUtil/src/sonia/cache/CacheInjector.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2009, Sebastian Sdorra
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of JAB; nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * http://kenai.com/projects/jab
+ * 
+ */
+
+package sonia.cache;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sonia.injection.Injector;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+public class CacheInjector implements Injector {
+
+    /** Field description */
+  private static Logger logger =
+    Logger.getLogger(CacheInjector.class.getName());
+
+  private CacheManager manager;
+
+  public CacheInjector( CacheManager manager )
+  {
+    this.manager = manager;
+  }
+
+
+
+
+
+  public void inject(Object object, Field field, Annotation annotation)
+  {
+    if ( annotation instanceof Cache )
+    {
+            Cache cache = (Cache) annotation;
+      String key = cache.value();
+      Class type = field.getType();
+      ObjectCache value = null;
+
+      if ( type.equals( ObjectCache.class ) )
+      {
+        value = manager.get(key);
+      }
+
+      if (value != null)
+      {
+        if (logger.isLoggable(Level.FINER))
+        {
+          StringBuffer msg = new StringBuffer();
+
+          msg.append("injecting cache").append(key);
+          msg.append(" in field ").append(field.getName());
+          logger.finer(msg.toString());
+        }
+
+        try
+        {
+          field.set(object, value);
+        }
+        catch (IllegalAccessException ex)
+        {
+          logger.log(Level.SEVERE, null, ex);
+        }
+      }
+    }
+  }
+
+}
