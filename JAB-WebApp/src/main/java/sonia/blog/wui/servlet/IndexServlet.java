c9742b0d88448768745a2a48a70ceb4d23791509
sdorra <sdorra@localhost>
2010-03-07 15:56:41 +0100

    replace forward.jsp with IndexServlet

---
+++ JAB-WebApp/src/main/java/sonia/blog/wui/servlet/IndexServlet.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2009, Sebastian Sdorra
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of JAB; nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * http://kenai.com/projects/jab
+ *
+ */
+
+
+
+package sonia.blog.wui.servlet;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import sonia.blog.api.app.BlogContext;
+import sonia.blog.api.app.BlogRequest;
+import sonia.blog.api.link.LinkBuilder;
+import sonia.blog.entity.Blog;
+import sonia.blog.util.BlogUtil;
+
+import sonia.util.Util;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Sebastian Sdorra
+ */
+public class IndexServlet extends HttpServlet
+{
+
+
+  //~--- methods --------------------------------------------------------------
+  /**
+   * Handles the HTTP <code>GET</code> method.
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request,
+                        HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
+
+  /**
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request,
+                                HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    response.setContentType("text/html;charset=UTF-8");
+
+    String uri = null;
+    BlogRequest blogRequest = BlogUtil.getBlogRequest(request);
+    LinkBuilder linkBuilder = BlogContext.getInstance().getLinkBuilder();
+
+    if (BlogContext.getInstance().isInstalled())
+    {
+      Blog blog = blogRequest.getCurrentBlog();
+
+      if ((blog != null) && Util.hasContent(blog.getStartPage()))
+      {
+        uri = blog.getStartPage();
+      }
+      else
+      {
+        uri = linkBuilder.buildLink(blogRequest, "/list/index.jab");
+      }
+    }
+    else
+    {
+      uri = linkBuilder.buildLink(blogRequest, "/install/step1.jab");
+    }
+
+    response.sendRedirect(uri);
+  }
+}
