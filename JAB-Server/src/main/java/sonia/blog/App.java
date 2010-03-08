3c7fa397fbcc5afc38eabda3e3a354e36e57853e
sdorra <sdorra@localhost>
2010-03-08 15:04:10 +0100

    added webstart plugin to JAB-Server

--- JAB-Server/src/main/java/sonia/blog/App.java
+++ JAB-Server/src/main/java/sonia/blog/App.java
@@ -37,6 +37,10 @@ public class App
    */
   public static void main(String[] args) throws Exception
   {
+
+    // disable security manager
+    System.setSecurityManager(null);
+
     final App app = new App();
     StringBuffer resourceBuffer = new StringBuffer();
 
