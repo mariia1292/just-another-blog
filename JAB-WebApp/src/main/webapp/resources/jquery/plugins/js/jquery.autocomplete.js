6ddd6a8238d48c8dc5d15e49327cae7574d89f7b
sdorra <sdorra@localhost>
2010-05-20 21:26:23 +0200

    improve jquery autocomplete plugin

--- JAB-WebApp/src/main/webapp/resources/jquery/plugins/js/jquery.autocomplete.js
+++ JAB-WebApp/src/main/webapp/resources/jquery/plugins/js/jquery.autocomplete.js
@@ -44,12 +44,13 @@
   $.Autocomplete = function( field, url, options )
   {
     var defaults = {
-      id: 'jqueryAutocomplete',
+      id: 'ja-' + createUUID(),
       maxItems : 10,
-      minChars : 2,
+      minChars : 3,
       formatter: defaultFormatter,
       queryParam: "query",
       processSelection: processSelection,
+      readValue: readValue,
       cache: true
     };
 
@@ -62,6 +63,7 @@
 
     var index = -1;
     var cache = null;
+    var timeout = null;
 
      if ( options.cache ){
        cache = [];
@@ -70,12 +72,17 @@
     $field.blur(function(){
       setTimeout( function(){
         $output.hide()
-      }, 200);
+      }, 300);
     });
 
     $field.keydown(function(event){
+      if ( timeout != null ){
+        clearTimeout( timeout );
+      }
       switch ( event.keyCode )
       {
+        // TAB
+        case 9:
         // ESC
         case 27:
           $output.hide();
@@ -92,21 +99,23 @@
         case 13:
           if ( selected != null ){
             event.preventDefault();
-            options.processSelection($field, selected);
+            options.processSelection($field, selected, getCaretPosition(field));
             $output.hide();
           }
           break;
+        // END
+        case 35:
+        // POS1
+        case 36:
         // LEFT
         case 37:
-           // do nothing
-           break;
         // Right
         case 39:
            // do nothing
            break;
         // ALL OTHER
         default:
-          setTimeout(processKeyPress, 10);
+          timeout = setTimeout(processKeyPress, 500);
       }
     });
 
@@ -139,11 +148,14 @@
     }
 
     function processKeyPress(){
-      var value = $field.val();
+      timeout = null;
+      var value = options.readValue( $field, $field.val(), getCaretPosition(field) );
       clear();
       if ( value.length >= options.minChars ){
         if ($output.length == ""){
           createOutputContainer();
+        } else {
+          setOutputPosition();
         }
         $output.empty().show();
 
@@ -151,6 +163,10 @@
         
         processRequest( url );
       }
+    }
+
+    function readValue( field, value ){
+      return value;
     }
 
     function processRequest(url){
@@ -176,9 +192,10 @@
     }
 
     function processResult(result){
+      clear();
       $.each(result, function(index, content){
         if ( index < options.maxItems ){
-          $output.append( $("<li />").append( options.formatter($field, content) ) );
+          $output.append( $("<li />").append( options.formatter($field, content, getCaretPosition(field)) ) );
         }
       });
     }
@@ -199,21 +216,51 @@
       if ( index > 0 ){
         url = url.substring(0, index);
       }
-      url += "?" + param + "=" + encodeURIComponent(value);
+      url += "?" + param + "=" + escape(value);
       return url;
     }
 
     function createOutputContainer(){
+      $output = $("<ul />").attr("id", options.id).addClass( "autocomplete" ).width( $field.width() );
+      setOutputPosition();
+      $("body").append( $output );
+    }
+
+    function setOutputPosition(){
       var p = findPosition();
-      $("body").append( $("<ul />").attr("id", options.id).addClass( "autocomplete" ).css( "top",  p.top + "px").css( "left", p.left + "px" ) );
-      $output = $("#" + options.id);
-      $output.width( $field.width() );
+      $output.css( "top",  p.top + "px").css( "left", p.left + "px" );
     }
 
     function findPosition(){
       var p = $field.offset();
       p.top += $field.outerHeight() - 1;
       return p;
+    }
+
+    function getCaretPosition (ctrl) {
+      var CaretPos = 0;
+      // IE Support
+      if (document.selection) {
+        ctrl.focus();
+        var Sel = document.selection.createRange ();
+        Sel.moveStart ('character', -ctrl.value.length);
+        CaretPos = Sel.text.length;
+      }
+      // Firefox support
+      else if (ctrl.selectionStart || ctrl.selectionStart == '0')
+        CaretPos = ctrl.selectionStart;
+
+      return (CaretPos);
+    }
+
+    function createUUID(){
+      var s = [], itoh = '0123456789ABCDEF';
+      for (var i = 0; i <36; i++) s[i] = Math.floor(Math.random()*0x10);
+      s[14] = 4;
+      s[19] = (s[19] & 0x3) | 0x8;
+      for (var i = 0; i <36; i++) s[i] = itoh[s[i]];
+        s[8] = s[13] = s[18] = s[23] = '-';
+      return s.join('');
     }
 
   }
